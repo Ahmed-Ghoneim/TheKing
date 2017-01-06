28e94a0b817de89ee0f552bfd37ea1856a05838d
Ahmed Ghoneim <>
2017-01-06 16:09:23 +0200

    game is on

--- src/Deck.java
+++ src/Deck.java
@@ -2,30 +2,28 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
-import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.layout.FlowPane;
 
 class Deck {
 
-    Random random = new Random();
-    private final int numSuits = 4;
-    private final int numRanks = 13;
+    private Random random = new Random();
+    private final int NUMBER_OF_SUITS = 4;
+    private final int NUMBER_OF_RANKS = 13;
 
-    ArrayList<Card> cards = new ArrayList();
+    private ArrayList<Card> cards = new ArrayList();
 
     public Deck() {
 
-        for (int i = 1; i <= numSuits; i++) {
-            for (int j = 1; j <= numRanks; j++) {
+        for (int i = 1; i <= NUMBER_OF_SUITS; i++) {
+            for (int j = 1; j <= NUMBER_OF_RANKS; j++) {
 
                 Card card = new Card(i, j);
                 cards.add(card);
+                
             }
         }
-        System.out.println("Deck created");
-
     }
 
     public Card distributeCard(int x) {
@@ -37,11 +35,11 @@ class Deck {
     }
 
     public void getKingReady() {
+        
         int deletedSuit = random.nextInt(3) + 1;
         Card card = new Card(deletedSuit, 13);
         if (cards.contains(card)) {
             cards.remove(card);
-            System.out.println("Deleted");
         }
 
     }
@@ -52,21 +50,18 @@ class Deck {
         flowPane.setPadding(new Insets(10, 10, 10, 10));
         flowPane.setAlignment(Pos.CENTER);
         flowPane.setHgap(-25);
-        ObservableList list = flowPane.getChildren();
-        for (Card c : cards) {
-
-            list.add(c.FRONT_VIEW);
-        }
+        
+        cards.forEach((c) -> {
+            flowPane.getChildren().add(c.FRONT_VIEW);
+        });
         flowPane.setMaxSize(700, 700);
         return flowPane;
     }
 
     public void deckShuffle() {
-
         for (int i = 0; i < 10; i++) {
             Collections.shuffle(cards);
         }
-
     }
 
     public int getSize() {
@@ -75,7 +70,5 @@ class Deck {
 
     public boolean isEmpty() {
         return cards.size() <= 0;
-    }
-
-    
+    }  
 }
